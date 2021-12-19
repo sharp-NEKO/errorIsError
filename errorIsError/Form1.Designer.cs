5e592756d427a76581e13a6c80ab32cec8f2e481
sharp_NEKO <65962822+sharp-NEKO@users.noreply.github.com>
2021-12-19 15:20:49 +0900

    Add Files

---
+++ errorIsError/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace errorIsError
+{
+    partial class KoizumiExe
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.koizumiImg = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.koizumiImg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // koizumiImg
+            // 
+            this.koizumiImg.Image = global::errorIsError.Properties.Resources.koizumi_circle_mini;
+            this.koizumiImg.Location = new System.Drawing.Point(40, 36);
+            this.koizumiImg.Name = "koizumiImg";
+            this.koizumiImg.Size = new System.Drawing.Size(48, 48);
+            this.koizumiImg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.koizumiImg.TabIndex = 0;
+            this.koizumiImg.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(119, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Error is error";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(291, 97);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 30);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(137, 97);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(148, 30);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "OK";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // KoizumiExe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 134);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.koizumiImg);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "KoizumiExe";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Koizumi.exe";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.koizumiImg)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox koizumiImg;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ errorIsError/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace errorIsError
+{
+    public partial class KoizumiExe : Form
+    {
+        public KoizumiExe()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            System.Media.SystemSounds.Hand.Play();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string cmd = "";
+            string[] args = Environment.GetCommandLineArgs();
+            for (int i = 1; i < args.Length; i++)
+            {
+                if (1 < i)
+                {
+                    cmd += " ";
+                }
+                cmd += "\"" + args[i] + "\"";
+            }
+            System.Diagnostics.Process.Start(Application.ExecutablePath, cmd);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string cmd = "";
+            string[] args = Environment.GetCommandLineArgs();
+            for (int i = 1; i < args.Length; i++)
+            {
+                if (1 < i)
+                {
+                    cmd += " ";
+                }
+                cmd += "\"" + args[i] + "\"";
+            }
+            System.Diagnostics.Process.Start(Application.ExecutablePath, cmd);
+        }
+    }
+}
